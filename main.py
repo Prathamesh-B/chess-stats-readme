dc74c7e6b1266600fbc0d75106656b1f8b85aa9b
Prathamesh B <55992548+Prathamesh-B@users.noreply.github.com>
2021-07-14 11:52:26 +0530

    Add test

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import requests
 
 GH_TOKEN = os.getenv("INPUT_GH_TOKEN")
 CHESS_USERNAME = os.getenv("INPUT_CHESS_USERNAME")
+
+
+def get_stats():
+    response = requests.get(f"https://api.chess.com/pub/player/{CHESS_USERNAME}/stats")
+    if response.status_code == 200:
+        print("Work")
+
+
+if __name__ == "__main__":
+    get_stats()
+    print(CHESS_USERNAME)
